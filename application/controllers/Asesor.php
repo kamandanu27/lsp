d55d9befc8a38f11cfd47cf4a9b51ead7858bfb6
ezamatakoding <eza.matakoding.com>
2023-06-17 09:58:57 +0700

    pengecekan

--- application/controllers/Asesor.php
+++ application/controllers/Asesor.php
@@ -72,7 +72,7 @@ class Asesor extends CI_Controller {
 
 			$data = array(
 				'noregister_asesor'   			=> $this->input->post('noregister_asesor'),
-				'nik_asesor'   			=> $this->input->post('nik_asesor'),
+				'nik_asesor'   				=> $this->input->post('nik_asesor'),
 				'nama_asesor'   			=> $this->input->post('nama_asesor'),
 				'alamat_asesor'   			=> $this->input->post('alamat_asesor'),
 				'notlp_asesor'   			=> $this->input->post('notlp_asesor'),
@@ -107,34 +107,8 @@ class Asesor extends CI_Controller {
 			redirect(base_url('asesor'),'refresh');
 		}else{
 
-				$this->form_validation->set_rules('id_asesor', 'ID asesor', 'required',
-				array( 'required'  => '%s harus diisi!'));
 
-				if($_FILES["foto_asesor"]['name'] == ""){ //jika tidak ada upload foto
-
-					$data = array(
-						'id_asesor'				=> $this->input->post('id_asesor'),
-						'noregister_asesor'   			=> $this->input->post('noregister_asesor'),
-						'nik_asesor'   			=> $this->input->post('nik_asesor'),
-						'nama_asesor'   			=> $this->input->post('nama_asesor'),
-						'alamat_asesor'   			=> $this->input->post('alamat_asesor'),
-						'notlp_asesor'   			=> $this->input->post('notlp_asesor'),
-						'jeniskelamin_asesor'     => $this->input->post('jeniskelamin_asesor'),
-						'agama_asesor'   			=> $this->input->post('agama_asesor'),
-						'tempatlahir_asesor'   			=> $this->input->post('tempatlahir_asesor'),
-						'tanggallahir_asesor'   			=> $this->input->post('tanggallahir_asesor'),
-						'provinsi_asesor'   			=> $this->input->post('provinsi_asesor'),
-						'kotakab_asesor'   			=> $this->input->post('kotakab_asesor'),
-						'jurusan_asesor'   			=> $this->input->post('jurusan_asesor'),
-						'programstudi_asesor'   			=> $this->input->post('programstudi_asesor'),
-						'email_asesor'   			=> $this->input->post('email_asesor'),
-						'password_asesor'   			=> $this->input->post('password_asesor'),
-						'sertifikat_asesor'   			=> $this->input->post('sertifikat_asesor')
-						);
-
-						$q = $this->asesor->update($data);
-
-				}else{//jika ada upload foto
+				if($_FILES["foto_asesor"]['name'] !== ""){ //jika tidak ada upload foto
 
 					$image 								= time().'-'.$_FILES["foto_asesor"]['name']; //data post dari form
 					$config['upload_path'] 				= './public/image/upload/asesor/'; //lokasi folder foto produk
@@ -147,32 +121,37 @@ class Asesor extends CI_Controller {
 
 						$data = array(
 							'id_asesor'				=> $this->input->post('id_asesor'),
-							'noregister_asesor'   			=> $this->input->post('noregister_asesor'),
-							'nik_asesor'   			=> $this->input->post('nik_asesor'),
-							'nama_asesor'   			=> $this->input->post('nama_asesor'),
-							'alamat_asesor'   			=> $this->input->post('alamat_asesor'),
-							'notlp_asesor'   			=> $this->input->post('notlp_asesor'),
-							'jeniskelamin_asesor'     => $this->input->post('jeniskelamin_asesor'),
-							'agama_asesor'   			=> $this->input->post('agama_asesor'),
-							'tempatlahir_asesor'   			=> $this->input->post('tempatlahir_asesor'),
-							'tanggallahir_asesor'   			=> $this->input->post('tanggallahir_asesor'),
-							'provinsi_asesor'   			=> $this->input->post('provinsi_asesor'),
-							'kotakab_asesor'   			=> $this->input->post('kotakab_asesor'),
-							'jurusan_asesor'   			=> $this->input->post('jurusan_asesor'),
-							'programstudi_asesor'   			=> $this->input->post('programstudi_asesor'),
-							'email_asesor'   			=> $this->input->post('email_asesor'),
-							'password_asesor'   			=> $this->input->post('password_asesor'),
-							'foto_asesor'		=> $image,
-							'sertifikat_asesor'   			=> $this->input->post('sertifikat_asesor')
+							'foto_asesor'		=> $image
 
 						);
 						
 						$q = $this->asesor->update($data);
 			
-					$this->session->set_flashdata('success', '<i class="fa fa-check"></i> Selamat! Data Berhasil Dirubah');
-					$this->asesor->update($data);
-					redirect(base_url('asesor'),'refresh');
 				}
+
+				$data = array(
+					'id_asesor'				=> $this->input->post('id_asesor'),
+					'noregister_asesor'   			=> $this->input->post('noregister_asesor'),
+					'nik_asesor'   			=> $this->input->post('nik_asesor'),
+					'nama_asesor'   			=> $this->input->post('nama_asesor'),
+					'alamat_asesor'   			=> $this->input->post('alamat_asesor'),
+					'notlp_asesor'   			=> $this->input->post('notlp_asesor'),
+					'jeniskelamin_asesor'     => $this->input->post('jeniskelamin_asesor'),
+					'agama_asesor'   			=> $this->input->post('agama_asesor'),
+					'tempatlahir_asesor'   			=> $this->input->post('tempatlahir_asesor'),
+					'tanggallahir_asesor'   			=> $this->input->post('tanggallahir_asesor'),
+					'provinsi_asesor'   			=> $this->input->post('provinsi_asesor'),
+					'kotakab_asesor'   			=> $this->input->post('kotakab_asesor'),
+					'jurusan_asesor'   			=> $this->input->post('jurusan_asesor'),
+					'programstudi_asesor'   			=> $this->input->post('programstudi_asesor'),
+					'email_asesor'   			=> $this->input->post('email_asesor'),
+					'password_asesor'   			=> $this->input->post('password_asesor'),
+					'sertifikat_asesor'   			=> $this->input->post('sertifikat_asesor')
+				);
+
+				$this->asesor->update($data);
+				$this->session->set_flashdata('success', '<i class="fa fa-check"></i> Selamat! Data Berhasil Dirubah');
+				redirect(base_url('asesor'),'refresh');
 			}
 	}
 
--- application/views/asesor/v_edit.php
+++ application/views/asesor/v_edit.php
@@ -44,8 +44,7 @@
                     <div class="row">
                         <div class="form-group">
                           <label class="col-md-4 control-label" for="noregister_asesor">No. Register</label>
-                          <div class="input-group col-md-8">
-                              <input class="form-control" type="hidden" id="id_asesor" name="id_asesor" value="<?= $data['id_asesor'] ?>" required>
+                          <div class="input-group col-md-7" id="id_asesor" name="id_asesor" value="<?= $data['id_asesor'] ?>" required>
                               <input type="number" class="form-control" placeholder="No. Register" name="noregister_asesor" value="<?= $data['noregister_asesor'] ?>" required>
                               <input type="hidden" name="<?=$this->security->get_csrf_token_name(); ?>" value="<?=$this->security->get_csrf_hash(); ?>" style="display: none">
                           </div>
@@ -53,119 +52,124 @@
 
                         <div class="form-group">
                           <label class="col-md-4 control-label" for="nik_asesor">Nik</label>
-                          <div class="input-group col-md-8">
+                          <div class="input-group col-md-7">
                               <input type="number" class="form-control" placeholder="Nik" name="nik_asesor" value="<?= $data['nik_asesor'] ?>" required>
                           </div>
                         </div>
 
                         <div class="form-group">
                           <label class="col-md-4 control-label" for="nama_asesor">Nama</label>
-                          <div class="input-group col-md-8">
+                          <div class="input-group col-md-7">
                               <input type="text" class="form-control" placeholder="Nama Asesor" name="nama_asesor" value="<?= $data['nama_asesor'] ?>" required>
                           </div>
                         </div>
 
                         <div class="form-group">
                           <label class="col-md-4 control-label" for="alamat_asesor">Alamat</label>
-                          <div class="input-group col-md-8">
+                          <div class="input-group col-md-7">
                               <input type="text" class="form-control" placeholder="Alamat" name="alamat_asesor" value="<?= $data['alamat_asesor'] ?>" required>
                           </div>
                         </div>
 
                         <div class="form-group">
                           <label class="col-md-4 control-label" for="notlp_asesor">No. Telp</label>
-                          <div class="input-group col-md-8">
+                          <div class="input-group col-md-7">
                               <input type="number" class="form-control" placeholder="No. Telp" name="notlp_asesor" value="<?= $data['notlp_asesor'] ?>" required>
                           </div>
                         </div>
 
                         <div class="form-group">
 											  	<label class="col-md-4 control-label" for="exampleInputEmail1">Jenis Kelamin Pemohon:</label>
-                          <select class="input-group col-sm-8 col-md-8" id="jeniskelamin_asesor" name="jeniskelamin_asesor">
-                            <option value="">Pilih</option>
-                            <option value="Laki-Laki">Laki-laki</option>
-                            <option value="Perempuan">Perempuan</option>
-                          </select>
+                          <div class="input-group col-md-7">
+                              <select class="form-control" id="jeniskelamin_asesor" name="jeniskelamin_asesor">
+                                <option value="">Pilih</option>
+                                <option value="Laki-Laki">Laki-laki</option>
+                                <option value="Perempuan">Perempuan</option>
+                              </select>
+                          </div>
+                          
 							         	</div>
 
                         <div class="form-group">
                           <label class="col-md-4 control-label" for="exampleInputEmail1">Agama Pemohon:</label>
-                          <select class="input-group col-sm-8 col-md-8" id="agama_asesor" name="agama_asesor">
-                            <option value="">Pilih</option>
-                            <option value="Islam">Islam</option>
-                            <option value="Kristen">Kristen</option>
-                            <option value="Katholik">Katholik</option>
-                            <option value="Hindu">Hindu</option>
-                            <option value="Budha">Budha</option>
-                          </select>
+                          <div class="input-group col-md-7">
+                              <select class="form-control" id="agama_asesor" name="agama_asesor">
+                                <option value="">Pilih</option>
+                                <option value="Islam">Islam</option>
+                                <option value="Kristen">Kristen</option>
+                                <option value="Katholik">Katholik</option>
+                                <option value="Hindu">Hindu</option>
+                                <option value="Budha">Budha</option>
+                              </select>
+                          </div>
 							        	</div>
 
                         <div class="form-group">
                           <label class="col-md-4 control-label" for="tempatlahir_asesor">Tempat Lahir</label>
-                          <div class="input-group col-md-8">
+                          <div class="input-group col-md-7">
                               <input type="text" class="form-control" placeholder="Tempat Lahir" name="tempatlahir_asesor" value="<?= $data['tempatlahir_asesor'] ?>" required>
                           </div>
                         </div>
 
                         <div class="form-group">
                           <label class="col-md-4 control-label" for="tanggallahir_asesor">Tanggal Lahir</label>
-                          <div class="input-group col-md-8">
+                          <div class="input-group col-md-7">
                               <input type="date" class="form-control" placeholder="Tanggal Lahir" name="tanggallahir_asesor" value="<?= $data['tanggallahir_asesor'] ?>" required>
                           </div>
                         </div>
 
                         <div class="form-group">
                           <label class="col-md-4 control-label" for="provinsi_asesor">Provinsi</label>
-                          <div class="input-group col-md-8">
+                          <div class="input-group col-md-7">
                               <input type="text" class="form-control" placeholder="Provinsi" name="provinsi_asesor" value="<?= $data['provinsi_asesor'] ?>" required>
                           </div>
                         </div>
 
                         <div class="form-group">
                           <label class="col-md-4 control-label" for="kotakab_asesor">Kota / Kabupaten</label>
-                          <div class="input-group col-md-8">
+                          <div class="input-group col-md-7">
                               <input type="text" class="form-control" placeholder="Kota / Kabupaten" name="kotakab_asesor" value="<?= $data['kotakab_asesor'] ?>" required>
                           </div>
                         </div>
 
                         <div class="form-group">
                           <label class="col-md-4 control-label" for="jurusan_asesor">Jurusan</label>
-                          <div class="input-group col-md-8">
+                          <div class="input-group col-md-7">
                               <input type="text" class="form-control" placeholder="Jurusan" name="jurusan_asesor" value="<?= $data['jurusan_asesor'] ?>" required>
                           </div>
                         </div>
 
                         <div class="form-group">
                           <label class="col-md-4 control-label" for="programstudi_asesor">Program Studi</label>
-                          <div class="input-group col-md-8">
+                          <div class="input-group col-md-7">
                               <input type="text" class="form-control" placeholder="Program Studi" name="programstudi_asesor" value="<?= $data['programstudi_asesor'] ?>" required>
                           </div>
                         </div>
 
                         <div class="form-group">
                           <label class="col-md-4 control-label" for="email_asesor">Email</label>
-                          <div class="input-group col-md-8">
+                          <div class="input-group col-md-7">
                               <input type="email" class="form-control" placeholder="Email" name="email_asesor" value="<?= $data['email_asesor'] ?>" required>
                           </div>
                         </div>
 
                         <div class="form-group">
                           <label class="col-md-4 control-label" for="password_asesor">Password</label>
-                          <div class="input-group col-md-8">
+                          <div class="input-group col-md-7">
                               <input type="text" class="form-control" placeholder="Password" name="password_asesor" value="<?= $data['password_asesor'] ?>" required>
                           </div>
                         </div>
 
                         <div class="form-group">
                             <label class="col-sm-4 col-form-label">Foto Asesor</label>
-                            <div class="input-group col-sm-8 col-md-8">
+                            <div class="input-group col-sm-8 col-md-7">
                              <input type="file" class="form-control" id="foto_asesor" name="foto_asesor" id="exampleInputUpload Foto1" placeholder="Upload Foto">
                             </div>
                         </div>
 
                         <div class="form-group">
                           <label class="col-md-4 control-label" for="sertifikat_asesor">Sertifikat</label>
-                          <div class="input-group col-md-8">
+                          <div class="input-group col-md-7">
                               <input type="text" class="form-control" placeholder="Sertifikat" name="sertifikat_asesor" value="<?= $data['sertifikat_asesor'] ?>" required>
                           </div>
                         </div>
@@ -177,7 +181,7 @@
                     <div class="form-group">
                       <div class="row">
                         <label class="col-md-4 control-label" for="name"></label>
-                        <div class="input-group col-md-8">
+                        <div class="input-group col-md-7">
                           <button type="submit" class="btn btn-primary" style="margin-right: 6px;">Update</button>
                           <a href="<?= base_url() ?>asesor" class="btn btn-danger">Batal</a>
                         </div>
